fa06d5e6bf00f38bff96c6a7f985a3216faa33e0
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2015-11-13 23:32:35 +0900

    Add member array stringId,ableNull,paramType and selectId of Array

--- source/js/AddedData.js
+++ source/js/AddedData.js
@@ -1,3 +1,52 @@
 var AddedData=function(Parser){
-	
+	if(Parser==null)
+		return;
+	this.data={"table":"rent"};
+
+	this.stringId=[
+		"name","evaluation","cost","deposit",
+		"reward","prefecture",
+		"city","ward","address",
+		"toSumi","toHigo","pair",
+		"bath_wc","balcony","two_stove",
+		"bicycles","merit","demerit",
+		"other","url"
+	];
+
+	this.ableNull=[
+		false,true,false,false,
+		false,false,
+		false,true,false,
+		false,false,true,
+		true,true,true,
+		true,true,true,
+		true,false
+	];
+
+	this.paramType=[
+		"String","Int","String","String",
+		"String","String",
+		"String","String","String",
+		"Int","Int","Int",
+		"Int","Int","Int",
+		"Int","String","String",
+		"String","String"
+	];
+
+	this.selectId=["prefecture","city","ward"];
+}
+
+AddedData.prototype.addData=function(key,value){
+	var index=-1;
+	for(var i=0;i<this.stringId.length;i++){
+		if(key===this.stringId[i]){
+			index=i;
+			break;
+		}
+	}
+	this.data[key]=value+","+this.paramType[index];
+}
+
+AddedData.prototype.setName=function(name){
+	this.data['name']=name;
 }
